42a2621a138a4e1af750dbcf80fbf3ac87fb068b
Lqm26 <825372764@qq.com>
2024-06-25 21:05:51 +0800

    Update train_ours.sh

--- scripts/train_ours.sh
+++ scripts/train_ours.sh
@@ -1,13 +1,13 @@
 # You should first set the path to your dataset and the output path in the config, and then run the following command based on your needs.
 BMCNet:
-eventzoom/nfs/RGB
+EventNFS/nfs-syn/RGB-syn
 python train.py -c config/train_EventZoom.yml
 python train.py -c config/train_nfs.yml
 python train.py -c config/train_RGB.yml
 
 
 BMCNet_plain:
-eventzoom/nfs/RGB
+EventNFS/nfs-syn/RGB-syn
 python train_plain.py -c config/train_EventZoom.yml
 python train_plain.py -c config/train_nfs.yml
 python train_plain.py -c config/train_RGB.yml
